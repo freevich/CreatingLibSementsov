83e23ae335c54618026b3640d65cb4b8b3e9f4c5
Oleg F Oganesyan <42918617+freevich@users.noreply.github.com>
2025-03-07 13:19:29 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+/*
+let client = {
+    fio: '',
+    dob: '',
+    age: 100,
+}
+*/
+let Instance = struct.createStructure();
+//console.log('hi! ', Instance.number);
+//
+let numb = struct.createNumber(null);
+console.log('numb: ', numb);
---
+++ src/struct.js
@@ -0,0 +1,18 @@
+//
+let struct = {
+    string: 'string',
+    createNumber: function (ref) {
+        //
+        let nu = new Number();
+        return nu =100;
+    },
+    boolean:  false,
+    createStructure: function () {
+        //
+        return this;
+    },
+    initStructure: function (obj={}) {
+        //
+
+    },
+};
